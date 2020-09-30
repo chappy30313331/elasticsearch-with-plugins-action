9eeb7606ef7a658bd2384dafdf4fdabf0078d82e
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2020-09-30 17:17:34 +0900

    chmod +x run-elasticsearch.sh

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM docker:stable
-
 RUN apk add --update bash
-
 COPY run-elasticsearch.sh /run-elasticsearch.sh
-
+RUN ["chmod", "+x", "/run-elasticsearch.sh"]
 ENTRYPOINT ["/run-elasticsearch.sh"]
--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -11,7 +11,7 @@ MAJOR_VERSION=`echo ${STACK_VERSION} | cut -c 1`
 
 PLUGIN_INSTALL_CMD=""
 PLUGINS_STR=`echo ${PLUGINS} | sed -e 's/\n/ /g'`
-if [ -n "${PLUGINS_STR" ]; then
+if [ -n "${PLUGINS_STR}" ]; then
   ARRAY=(${PLUGINS_STR})
   for i in "${ARRAY[@]}"
   do
